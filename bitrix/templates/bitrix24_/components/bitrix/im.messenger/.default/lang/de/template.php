d36cb4e51fb29640f2fbc6dccb389e546ed5ec19
MrCrooK <admin@msite.top>
2016-05-27 17:21:07 +0500

    remove old templatets

--- bitrix/templates/bitrix24_/components/bitrix/im.messenger/.default/lang/de/template.php
+++
@@ -1,9 +0,0 @@
-<?
-$MESS["IM_MESSENGER_OPEN_MESSENGER_CP"] = "Web-Messenger öffnen";
-$MESS["IM_MESSENGER_OPEN_NOTIFY"] = "Benachrichtigungen anzeigen";
-$MESS["IM_MESSENGER_OPEN_EMAIL"] = "E-Mail anzeigen";
-$MESS["IM_MESSENGER_OPEN_LF"] = "Activity Stream anzeigen";
-$MESS["IM_MESSENGER_OPEN_CALL2"] = "Anruf machen";
-$MESS["IM_MESSENGER_OPEN_NETWORK"] = "In Bitrix24.Network wechseln";
-$MESS["IM_MESSENGER_OPEN_MESSENGER"] = "Chat und Anrufe öffnen";
-?>
